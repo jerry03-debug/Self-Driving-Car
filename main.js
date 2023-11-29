496a928af22285bcad2f76f317008c514035581d
Diery Dia <dierydia@esp.sn>
2023-11-29 00:47:34 +0000

    the car is actually moving !!

--- main.js
+++ main.js
@@ -1,1 +1,20 @@
-alert("ehhh")
+const canvas = document.getElementById("myCanvas")
+canvas.height  = window.innerHeight;
+canvas.width = 200;
+
+const ctx = canvas.getContext("2d");
+car = new Car(100,200,30,50);
+car.draw(ctx)
+
+
+
+animate();
+
+function animate() {
+    car.update();
+    canvas.height  = window.innerHeight;
+    car.draw(ctx)
+
+    requestAnimationFrame(animate)
+    
+}
